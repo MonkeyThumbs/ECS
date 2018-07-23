51f64d5c8557d09a987e4060107d163fc968aa88
MonkeyThumbs <joshuajwiseman@gmail.com>
2018-07-23 17:24:43 -0400

    Update Entity.h

--- Include/Entity.h
+++ Include/Entity.h
@@ -6,6 +6,7 @@ namespace ecs{
 	using UID = std::bitset<64>;
 	using Handle = size_t;
 	using Keybits = std::bitset<64>;
+	using Lockbits = std::bitset<64>;
 	constexpr size_t NULL_ENTITY(0);
 	constexpr size_t MAX_ENTITIES{ 2000 };
 	}
